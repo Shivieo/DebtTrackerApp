bd0243bf73f7b20a77c170af0ffb126672d45ed7
Shivam Barnwal <shivambarnwal983@gmail.com>
2026-03-06 13:36:24 +0530

    added functions in advance debt file.. work in progress

--- app/src/main/java/com/example/debttrackerapp/total_debt_simple/AdvanceDebt.kt
+++ app/src/main/java/com/example/debttrackerapp/total_debt_simple/AdvanceDebt.kt
@@ -1,31 +1,187 @@
 package com.example.debttrackerapp.total_debt_simple
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AdvanceDebtTracker(navController: NavController) {
+
+    // States for dropdowns
+    var expanded by remember { mutableStateOf(false) }
+    val optionsInc = listOf("Credit Purchase", "Assets purchase on credit", "Outstanding Salary", "Outstanding Rent", "Miscellaneous outstandings")
+    val optionsDec = listOf("Sales", "Miscellaneous sales")
+    val optionsExp = listOf("Rent", "Electricity", "Water", "Miscellaneous expenses")
+    var selectedOptionInc by remember { mutableStateOf(optionsInc[0]) }
+    var selectedOptionDec by remember { mutableStateOf(optionsDec[0]) }
+    var selectedOptionsExp by remember { mutableStateOf(optionsExp[0]) }
+
+    var amountInput by remember { mutableStateOf("") }
+
     Column(
-        modifier = Modifier.fillMaxSize()
-            .background(color = Color.Cyan),
+        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("Hello this is for testing for Advance Debt Tracker")
 
-        Spacer(Modifier.height(25.dp))
+        Text("Advanced Debt Entry", style = MaterialTheme.typography.headlineMedium)
+
+        Spacer(Modifier.height(10.dp))
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            OutlinedTextField(
+                value = selectedOptionInc,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text("Category") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier
+                    .menuAnchor(type = MenuAnchorType.PrimaryNotEditable, enabled = true)
+                    .fillMaxWidth()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                optionsInc.forEach { optionsInc ->
+                    DropdownMenuItem(
+                        text = { Text(optionsInc) },
+                        onClick = {
+                            selectedOptionInc = optionsInc
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+
+        Spacer(Modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            label = { Text("Enter Amount") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(10.dp))
+
+        // second
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            OutlinedTextField(
+                value = selectedOptionDec,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text("Category") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier
+                    .menuAnchor(type = MenuAnchorType.PrimaryNotEditable, enabled = true)
+                    .fillMaxWidth()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                optionsInc.forEach { optionsDec ->
+                    DropdownMenuItem(
+                        text = { Text(optionsDec) },
+                        onClick = {
+                            selectedOptionInc = optionsDec
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+
+        Spacer(Modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            label = { Text("Enter Amount") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(10.dp))
+
+        // third
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            OutlinedTextField(
+                value = selectedOptionsExp,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text("Category") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier
+                    .menuAnchor(type = MenuAnchorType.PrimaryNotEditable, enabled = true)
+                    .fillMaxWidth()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                optionsInc.forEach { optionsExp ->
+                    DropdownMenuItem(
+                        text = { Text(optionsExp) },
+                        onClick = {
+                            selectedOptionsExp = optionsExp
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+
+        Spacer(Modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            label = { Text("Enter Amount") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(10.dp))
 
         Button(
             onClick = {
